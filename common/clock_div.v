47d0f039d89aa5698907fb99def02bcfad1077b2
Gabriel Rodríguez Canal <gabriel.rodriguez.canal@gmail.com>
2020-09-26 19:48:27 +0100

    Deleted legacy dead line

--- common/clock_div.v
+++ common/clock_div.v
@@ -5,8 +5,6 @@ module clock_div(
 	parameter DIVIDER = 50000000; // frequency of my FPGA oscillator. This is hardcoded for personal use
 	
 	reg [30:0] counter;
-	reg [30:0] thresh_freq; // maximum number of ticks until first positive flank of out_clk
-
 	
 	always @(posedge in_clk) begin
 		counter <= counter + 30'd1;
